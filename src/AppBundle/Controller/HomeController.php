d9a1919f54d1e88599a9af530aa56e4513a10f05
evaneox <csfrance_noob@hotmail.fr>
2017-10-02 23:29:14 +0200

    refactor (controller): Simulate homepage custom build

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -2,9 +2,10 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Translation\Translator;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 /**
@@ -25,6 +26,17 @@ class HomeController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('default/index.html.twig');
+        dump($request->getUriForPath('/uploads/myimage.jpg'));
+
+        return $this->render('homepage.html.twig', [
+            'page' => [
+                'title'         => $this->get('translator')->trans('homepage_title'),
+                'meta'          => [
+                    'description'   => $this->get('translator')->trans('homepage_meta_description'),
+                ],
+                'headerClass'   => 'full-height bg2',
+            ]
+        ]);
     }
 }
+
