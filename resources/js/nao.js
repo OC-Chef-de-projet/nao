3198522a7d47ddf01e8387918e154054e11bb358
evaneox <csfrance_noob@hotmail.fr>
2017-10-05 19:37:41 +0200

    Fix (*): Pull page title in navbar when header is collapse for mobile

--- resources/js/nao.js
+++ resources/js/nao.js
@@ -91,12 +91,10 @@
                  */
                 if(positionMainTitleToTop <= 0){
                     navLogo.addClass('activated');
-                    navSmallTitle.html(PageTitle.html());
                     navSmallTitle.addClass('activated');
                     PageTitle.addClass('activated');
                 }else{
                     navLogo.removeClass('activated');
-                    navSmallTitle.html('');
                     navSmallTitle.removeClass('activated');
                     PageTitle.removeClass('activated');
                 }
