0194f4d403c1e91b27536cd1ca4c77f0bd93c44c
evaneox <csfrance_noob@hotmail.fr>
2017-10-05 21:56:31 +0200

    Feat (Observation): Integration observation page (map/lis/detail)

---
+++ src/AppBundle/Controller/ObservationController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class ObservationController extends Controller
+{
+    /**
+     * @Route("/observation/creation", name="observation.create")
+     * @Method({"GET","POST"})
+     */
+    public function CreateAction(Request $request)
+    {
+        return $this->render('observation/create.html.twig');
+    }
+
+    /**
+     * @Route("/observation/carte", name="observation.map")
+     * @Method({"GET"})
+     */
+    public function showMapAction()
+    {
+        return $this->render('observation/map.html.twig');
+    }
+
+    /**
+     * @Route("/observation/carte/{id}", name="observation.detail", requirements={"id": "\d+"})
+     * @Method({"GET"})
+     */
+    public function showDetailAction(Request $request)
+    {
+        return $this->render('observation/detail.html.twig');
+    }
+
+    /**
+     * @Route("/observation/liste", name="observation.list")
+     * @Method({"GET"})
+     */
+    public function showListAction()
+    {
+        return $this->render('observation/list.html.twig');
+    }
+}
