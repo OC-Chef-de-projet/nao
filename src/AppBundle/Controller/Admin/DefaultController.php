b268ae1e1d549293b65e3a62af286ad40bcda5bd
Pierre-Sylvain Augereau <ps.augereau@gmail.com>
2017-10-06 22:29:41 +0200

    refactor (admin): use annotation for routing

--- src/AppBundle/Controller/Admin/DefaultController.php
+++ src/AppBundle/Controller/Admin/DefaultController.php
@@ -3,25 +3,32 @@
 namespace AppBundle\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
+
+/**
+ * Class DefaultController
+ *
+ * @Route("/admin")
+ *
+ * @package AppBundle\Controller\Admin
+ */
 class DefaultController extends Controller
 {
 
     /**
      * Dashboard form admin user
      *
+     * @Route("/", name="admin_homepage")
+     * @Method({"GET"})
+
+     *
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction()
     {
 
-        return $this->render('admin\index.html.twig',[
-            'header' => [
-                'bodyClass' => 'background-2',
-                'breadcrumb' => $this->container->get('app.admin')->getHomeBreadcrumb(),
-                'notification' => $this->container->get('app.notification')->count(),
-                'notifications' =>   $this->container->get('app.notification')->getNotifications()
-            ]
-        ]);
+        return $this->render('admin\index.html.twig');
     }
 }
