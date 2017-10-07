c81a00187dce47e37f145550deb4e2e349803bb0
evaneox <csfrance_noob@hotmail.fr>
2017-10-07 22:50:15 +0200

    Feat (Newsletter): Create newsletter API / Service

---
+++ src/AppBundle/Controller/API/NewsletterController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller\API;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Class BlogController
+ * @package AppBundle\Controller
+ * @Route("/api/newsletter")
+ */
+class NewsletterController extends Controller
+{
+
+    /**
+     * @Route("/subscribe", name="newsletter.subscribe")
+     * @Method({"POST"})
+     */
+    public function subscribeAction(Request $request)
+    {
+        return new JsonResponse(['valid' => false]);
+    }
+
+
+}
