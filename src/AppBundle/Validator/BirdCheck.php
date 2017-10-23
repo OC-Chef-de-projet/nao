3cabb044925afd0b1cf7f47fa5e86eca24068655
evaneox <csfrance_noob@hotmail.fr>
2017-10-23 22:00:50 +0200

    Feat (Observation): Create custom validator for taxref name

---
+++ src/AppBundle/Validator/BirdCheck.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Validator;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+class BirdCheck extends Constraint
+{
+    public $message  = "bird.wrong";
+
+    public function validatedBy()
+    {
+        return 'app.bird.check';
+    }
+
+}
---
+++ src/AppBundle/Validator/BirdCheckValidator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Validator;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+
+class BirdCheckValidator extends ConstraintValidator
+{
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function validate($name, Constraint $constraint)
+    {
+        $bird = $this->em->getRepository('AppBundle:Taxref')->findOneBy(array('common_name' => $name));
+
+        if(!$bird || !empty(trim($name))) {
+            $this->context->buildViolation($constraint->message)->addViolation();
+        }
+    }
+}
